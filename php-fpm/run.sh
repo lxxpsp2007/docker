18c89cdd773f6df9c45bdab01466dad72cadc72b
lxxpsp2007 <lxxpsp2007@126.com>
2016-12-08 15:53:55 +0800

    change php56 $fpm_port

---
+++ php-fpm/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+php_version=php-5.6.28
+
+if [ ! -z "${fpm_port}" ];then
+    sed -i "s/^listen = .*/listen = 0.0.0.0:${fpm_port}/g" /usr/local/${php_version}/etc/php-fpm.conf
+fi
+
+/usr/local/php-5.6.28/sbin/php-fpm -F 
